5646b4177c2daff8aa4e5e6145d4b773022a2d19
smuke <smuke@smuke.dev>
2024-07-21 15:32:29 -0700

    Update for Windows

--- DiscordForceTheme.java
+++ DiscordForceTheme.java
@@ -2,68 +2,56 @@ import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class DiscordForceTheme
 {
-    final static String ADD_CLASS_JS = "document.body.classList.add(\"theme-midnight\");";
-    final static String VERSION_DIRECTORY_REGEX = "\\d+\\.\\d+\\.\\d+";
+    protected final static String ADD_CLASS_JS = "document.body.classList.add(\"theme-midnight\");";
+    protected static String versionDirectoryRegex;
+    protected static String dataDirectory;
+    protected static File[] files;
+    protected static String indexFilePath;
 
     public static void main(String[] args)
     {
         Path path = Paths.get(".");
         Path absolutePath = path.toAbsolutePath().normalize();
-        String[] dirs = absolutePath.toString().split("/");
+        String[] dirs = absolutePath.toString().split(Pattern.quote(File.separator));
 
-        // /Users/name/Library/Application Support
+        // Windows
+        // C:\Users\name\AppData\Local\Discord\app-1.0.0\modules\discord_modules-1\discord_modules
         try
         {
-            String finalDir = Paths.get(dirs[0] + "/" + dirs[1] + "/" + dirs[2] + "/Library/Application Support/discord").toString();
-            File[] files = Objects.requireNonNull(new File(finalDir).listFiles());
+            dataDirectory = Paths.get(dirs[0] + "\\" + dirs[1] + "\\" + dirs[2] + "\\AppData\\Local\\Discord").toString();
+            files = Objects.requireNonNull(new File(dataDirectory).listFiles());
+            versionDirectoryRegex = "app-\\d+\\.\\d+\\.\\d+";
+            indexFilePath = "\\modules\\discord_modules-1\\discord_modules\\index.js";
 
-            Pattern pattern = Pattern.compile(VERSION_DIRECTORY_REGEX);
+            if (dataDirectory.isEmpty()) return;
 
-            for (File file : files)
-            {
-                String fileName = file.getName();
-                if (!pattern.matcher(fileName).matches()) continue;
-
-                File indexFile = new File(finalDir + "/" + fileName + "/modules/discord_modules/index.js");
-
-                System.out.println("Found file " + indexFile);
-
-                try
-                {
-                    Scanner scan = new Scanner(indexFile);
-                    scan.nextLine();
-
-                    if (scan.hasNextLine() && !scan.nextLine().isEmpty())
-                    {
-                        System.out.println("The index.js file has already been modified!");
-                        return;
-                    }
-
-                    System.out.println("Writing...");
-
-                    FileOutputStream outputStream = new FileOutputStream(indexFile, true);
-                    PrintWriter outFile = new PrintWriter(outputStream);
-
-                    outFile.write("\n" + ADD_CLASS_JS);
-
-                    System.out.println("Successfully added the theme code to " + indexFile);
-
-                    outFile.close();
-                }
-                catch (FileNotFoundException exception)
-                {
-                    System.out.println("An error occurred! File was not found.");
-                }
-            }
+            ForceThemeProcessor processor = new ForceThemeProcessor();
+            processor.process();
         }
         catch (Exception exception)
         {
-            System.out.println("An error occurred! Could not find the application directory.");
+            System.out.println("Could not find the application directory for Windows: " + dataDirectory);
+        }
+
+        // Mac
+        // /Users/name/Library/Application Support
+        try
+        {
+            dataDirectory = Paths.get(dirs[0] + "/" + dirs[1] + "/" + dirs[2] + "/Library/Application Support/discord").toString();
+            files = Objects.requireNonNull(new File(dataDirectory).listFiles());
+            versionDirectoryRegex = "\\d+\\.\\d+\\.\\d+";
+            indexFilePath = "/modules/discord_modules/index.js";
+
+            ForceThemeProcessor processor = new ForceThemeProcessor();
+            processor.process();
+        }
+        catch (Exception exception)
+        {
+            System.out.println("Could not find the application directory for Mac: " + dataDirectory);
         }
     }
 }
---
+++ ForceThemeProcessor.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class ForceThemeProcessor extends DiscordForceTheme
+{
+    private final Pattern pattern;
+
+    public ForceThemeProcessor()
+    {
+        pattern = Pattern.compile(versionDirectoryRegex);
+    }
+
+    public void process()
+    {
+        for (File file : files)
+        {
+            String fileName = file.getName();
+            if (!pattern.matcher(fileName).matches()) continue;
+
+            File indexFile = new File(dataDirectory + "/" + fileName + indexFilePath);
+
+            System.out.println("Found file " + indexFile);
+
+            try
+            {
+                Scanner scan = new Scanner(indexFile);
+                scan.nextLine();
+
+                if (scan.hasNextLine() && !scan.nextLine().isEmpty())
+                {
+                    System.out.println("The index.js file has already been modified!");
+                    return;
+                }
+
+                System.out.println("Writing...");
+
+                FileOutputStream outputStream = new FileOutputStream(indexFile, true);
+                PrintWriter outFile = new PrintWriter(outputStream);
+
+                outFile.write("\n" + ADD_CLASS_JS);
+
+                System.out.println("Successfully added the theme code to " + indexFile);
+
+                outFile.close();
+            }
+            catch (FileNotFoundException exception)
+            {
+                System.out.println("An error occurred! File was not found.");
+            }
+        }
+    }
+}
